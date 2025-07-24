d88432e179898001087479153e536196abc3d13e
ABHINAV8845 <2420030263@klh.edu.in>
2025-07-24 11:43:58 +0530

    Add files via upload

---
+++ skill1/autoboxingexample.java
@@ -0,0 +1,14 @@
+package skill1;
+
+public class autoboxingexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    int num=20;  
+		    System.out.println("original number"+num);
+		   Integer i=num; 
+		    Integer j=Integer.valueOf(num);
+		   
+		   System.out.println(num+" "+i+" "+j);  
+		  }
+		}  
---
+++ skill1/unboxingexample.java
@@ -0,0 +1,14 @@
+package skill1;
+
+public class unboxingexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double d=258.6;
+		double a=d;
+		double t=d.doubleValue();
+		System.out.println("orginal number "+d);
+		System.out.println(d+" "+a+" "+t);
+		}
+
+}
